62b0e7e4af618dabb965b25ce96c246402c1059f
ws <wshappyday@sina.com>
2023-09-02 20:56:26 +0800

    createApp

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+  props: ["title"],
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import {
+  createApp,
+  // ref,
+  // h
+} from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+createApp(App, { title: "WSCFAN" })
+  .provide("title", "hello world")
+  .mount("#app");
+// createApp({
+//   setup() {
+//     const name = ref("张飞");
+//     return () =>
+//       h("div", {
+//         innerHTML: `<h1>${name.value}来也`,
+//       });
+//   },
+// }).mount("#app");
